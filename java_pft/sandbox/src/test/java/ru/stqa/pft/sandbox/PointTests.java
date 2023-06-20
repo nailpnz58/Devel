5fb9276c5b534c35b7c63eb07af3d535828dcdaa
nailpnz58 <61517856+nailpnz58@users.noreply.github.com>
2023-06-20 10:02:25 +0300

    добавление второго теста задание 3

--- java_pft/sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ java_pft/sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -5,9 +5,14 @@ import org.testng.annotations.Test;
 @Test
 public class PointTests {
   public void testPoint() {
-  Point p1 = new Point(3, 4);
-  Point p2 = new Point(5, 6);
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(5, 6);
 
     Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
+
+    p1 = new Point(0, 0);
+    p2 = new Point(0, 0);
+
+    Assert.assertEquals(p1.distance(p2), 0.0);
   }
 }
