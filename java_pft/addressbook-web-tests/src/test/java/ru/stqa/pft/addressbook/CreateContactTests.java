c8cb1390f4fdc193e73db0979018dcc2a5ab26ef
nailpnz58 <61517856+nailpnz58@users.noreply.github.com>
2023-06-22 10:45:40 +0300

    Тест создания контакта

---
+++ java_pft/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTests.java
@@ -0,0 +1,93 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateContactTests {
+  private WebDriver wd;
+  
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
+    wd = new ChromeDriver();
+    login();
+  }
+  private void login() {
+    wd.get("http://localhost:8080/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    gotoAddNewContactPage();
+    initContactCreation();
+    fillContactForm();
+    submitContactCreation();
+    logout();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillContactForm() {
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys("Иван");
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys("Иванович");
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys("Иванов");
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys("Москва, ул. Красная, 45");
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys("+ 7 999 999 99 99");
+  }
+
+  private void initContactCreation() {
+    wd.findElement(By.name("firstname")).click();
+  }
+
+  private void gotoAddNewContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
+
